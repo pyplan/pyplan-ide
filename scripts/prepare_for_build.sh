6958bed8660b39d9b08508700df5eddd558b31b7
Fabian <fbrussa@novix.com>
2019-12-23 11:20:24 -0300

    build steps

--- scripts/prepare_for_build.sh
+++ scripts/prepare_for_build.sh
@@ -6,5 +6,9 @@ cp -R ../pyplan-engine/pyplan_engine .
 echo "Copy ui/dist files: (builded for desktop)"
 rm -rf ./pyplan/frontend
 mkdir -p ./pyplan/frontend
-cp -R ../../html/cubeplan-ui/dist/* ./pyplan/frontend
+# revisar paths
+#cd ../../html/cubeplan-ui
+#npm run build:desktop
+#cd ../../python/pyplan-ide
+#cp -R ../../html/cubeplan-ui/dist/* ./pyplan/frontend
 

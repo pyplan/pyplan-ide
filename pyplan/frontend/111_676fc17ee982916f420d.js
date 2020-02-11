caf9e26f7f1228c7fd68e76adb67014500027387
Facundo Bove <fjbove@gmail.com>
2020-02-11 17:05:59 -0300

    last open models

--- pyplan/frontend/111_676fc17ee982916f420d.js
+++
@@ -1,2 +0,0 @@
-/*! Copyright Pyplan 2020. All rights reserved. */
-(window.webpackJsonp=window.webpackJsonp||[]).push([[111],{1415:function(n,o,e){"use strict";(function(t){var c;void 0===(c=function(){return t.Controller.extend({name:"sessions",show:function(){e.e(215).then(function(){var n=[e(1847)];(function(n){(new n).render()}).apply(null,n)}).catch(e.oe)}})}.apply(o,[]))||(n.exports=c)}).call(this,e(694))}}]);

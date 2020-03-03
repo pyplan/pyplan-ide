9f398ee9fbfbe7f6eb74ca9d1b4444ddffa3249f
Facundo Bove <fjbove@gmail.com>
2020-03-03 11:03:34 -0300

    revert frontend

---
+++ pyplan/frontend/63_936613bc00bde44c4b76.js
@@ -0,0 +1,2 @@
+/*! Copyright Pyplan 2020. All rights reserved. */
+(window.webpackJsonp=window.webpackJsonp||[]).push([[63],{740:function(n,e,t){"use strict";(function(i){var o,r;o=[t(730)],void 0===(r=function(n){return n.extend({el:i("#n_o_n_e"),render:function(n){this.baseRender(n),this.drawItem()},drawItem:function(){}})}.apply(e,o))||(n.exports=r)}).call(this,t(1))}}]);

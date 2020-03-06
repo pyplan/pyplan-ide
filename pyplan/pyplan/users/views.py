424d8128517a9aa07a4ef1265a97b2c57a7eecfd
NCardCaptor <NCardCaptor@users.noreply.github.com>
2020-03-06 11:28:46 -0300

    fixes (#25)

--- pyplan/pyplan/users/views.py
+++ pyplan/pyplan/users/views.py
@@ -23,7 +23,7 @@ class UserViewSet(viewsets.ModelViewSet):
             service = UserService(request)
             response = service.list()
             if len(response) > 0:
-                return Response(UserSerializer(response, many=True).data, status=status.HTTP_200_OK)
+                return Response(UserSerializer(response, many=True, context={'request': request}).data, status=status.HTTP_200_OK)
             return Response(status=status.HTTP_204_NO_CONTENT)
         except Exception as ex:
             return Response(str({ex}), status=status.HTTP_406_NOT_ACCEPTABLE)
@@ -34,7 +34,18 @@ class UserViewSet(viewsets.ModelViewSet):
             if user_id:
                 service = UserService(request)
                 response = service.retrieve(user_id)
-                return Response(UserSerializer(response).data, status=status.HTTP_200_OK)
+                return Response(UserSerializer(response, context={'request': request}).data, status=status.HTTP_200_OK)
+            return Response("Could not find user id ", status=status.HTTP_406_NOT_ACCEPTABLE)
+        except Exception as ex:
+            return Response(str({ex}), status=status.HTTP_406_NOT_ACCEPTABLE)
+
+    def partial_update(self, request, *args, **kwargs):
+        try:
+            user_id = kwargs.get("pk")
+            if user_id:
+                service = UserService(request)
+                response = service.partial_update(user_id, request.data)
+                return Response(UserSerializer(response, context={'request': request}).data, status=status.HTTP_200_OK)
             return Response("Could not find user id ", status=status.HTTP_406_NOT_ACCEPTABLE)
         except Exception as ex:
             return Response(str({ex}), status=status.HTTP_406_NOT_ACCEPTABLE)

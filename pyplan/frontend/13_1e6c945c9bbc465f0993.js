8cdfaf99140fc4bf2ecce796d4c900151ce6fa22
Kylo <fjbove@gmail.com>
2020-04-17 10:30:49 -0300

    Updates (#41)
    * update engine
    
    * update UI

---
+++ pyplan/frontend/13_1e6c945c9bbc465f0993.js
@@ -0,0 +1,2 @@
+/*! Copyright Pyplan 2020. All rights reserved. */
+(window.webpackJsonp=window.webpackJsonp||[]).push([[13],{740:function(n,o,e){"use strict";(function(t){var a;void 0===(a=function(){return t.Controller.extend({name:"showLinkToDashboard",show:function(n,o){e.e(307).then(function(){var t=[e(2162)];(function(e){var t=new e(o);t.setElement(n),t.render()}).apply(null,t)}).catch(e.oe)}})}.apply(o,[]))||(n.exports=a)}).call(this,e(694))}}]);
--- pyplan/frontend/29_1f8065e848cd56eee935.js
+++
@@ -1,2 +0,0 @@
-/*! Copyright Pyplan 2020. All rights reserved. */
-(window.webpackJsonp=window.webpackJsonp||[]).push([[29],{881:function(n,o,e){"use strict";(function(t){var c;void 0===(c=function(){return t.Controller.extend({name:"Scenario",show:function(n){e.e(280).then(function(){var o=[e(2094)];(function(o){(new o).render(n)}).apply(null,o)}).catch(e.oe)},showDataSnapshot:function(){e.e(281).then(function(){var n=[e(2098)];(function(n){(new n).render()}).apply(null,n)}).catch(e.oe)}})}.apply(o,[]))||(n.exports=c)}).call(this,e(694))}}]);

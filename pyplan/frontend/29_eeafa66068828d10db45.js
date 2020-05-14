566d918eb6886b8505aa362bf20debbcff4c151c
Fabian <fbrussa@novix.com>
2020-05-14 17:02:20 -0300

    Update Frontend

---
+++ pyplan/frontend/29_eeafa66068828d10db45.js
@@ -0,0 +1,2 @@
+/*! Copyright Pyplan 2020. All rights reserved. */
+(window.webpackJsonp=window.webpackJsonp||[]).push([[29],{881:function(n,o,e){"use strict";(function(t){var c;void 0===(c=function(){return t.Controller.extend({name:"Scenario",show:function(n){e.e(283).then(function(){var o=[e(2099)];(function(o){(new o).render(n)}).apply(null,o)}).catch(e.oe)},showDataSnapshot:function(){e.e(284).then(function(){var n=[e(2103)];(function(n){(new n).render()}).apply(null,n)}).catch(e.oe)}})}.apply(o,[]))||(n.exports=c)}).call(this,e(694))}}]);

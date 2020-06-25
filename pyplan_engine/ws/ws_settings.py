98b5749e3814f0b7c9db7fae722aa62fa0a78a98
NCardCaptor <NCardCaptor@users.noreply.github.com>
2020-06-25 13:07:05 -0300

    engine changes (#52)

--- pyplan_engine/ws/ws_settings.py
+++ pyplan_engine/ws/ws_settings.py
@@ -5,3 +5,16 @@ class ws_settings:
     MSG_TYPE_ENTER = 3
     MSG_TYPE_PROGRESSBAR = 4  # For progressbar messages
     MSG_TYPE_DEBUG_MODE_INFO = 6  # For debug mode messages
+
+    # ### Notification Levels
+    NOTIFICATION_LEVEL_INFO = 0
+    NOTIFICATION_LEVEL_SUCCESS = 1
+    NOTIFICATION_LEVEL_ERROR = 2
+    NOTIFICATION_LEVEL_WARNING = 3
+
+    NOTIFICATION_LEVEL_CHOICES = (
+        (NOTIFICATION_LEVEL_INFO, 'info'),
+        (NOTIFICATION_LEVEL_SUCCESS, 'success'),
+        (NOTIFICATION_LEVEL_ERROR, 'error'),
+        (NOTIFICATION_LEVEL_WARNING, 'warning'),
+    )
